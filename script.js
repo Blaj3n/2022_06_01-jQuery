7bc057cb26ff1497d4ea777264ca0f38d3c0b5bb
Blaj3n <ulrichbence15@freemail.hu>
2022-06-01 11:04:22 +0200

    img + css

--- script.js
+++ script.js
@@ -4,8 +4,18 @@
 
 $(function () {
   console.log("valami");
-  $(".tartalom").html("<div>Első div tartalma")
-  $(".tartalom").append("<div>Második div tartalma")
-  let tartalom =$(".tartalom").text()
-  console.log(tartalom)
+  $(".tartalom").html("<div>Első div tartalma");
+  $(".tartalom").append("<div>Második div tartalma");
+  $(".tartalom").after("<img>");
+  $(".tartalom").after("<img>");
+  $(".tartalom").after("<img>");
+  $(".tartalom").after("<img>");
+  //   The eq() method returns an element with a specific index number of the selected elements.
+  // The index numbers start at 0, so the first element will have the index number 0 (not 1).
+  $("img").eq(0).attr("src", "img/motivation.jpg");
+  $("img").css({ width: "50%", height: "50%", border: "1px solid gray" });
+  $(".tartalom div").css("border", "1px solid gray");
+  $(".tartalom div").toggleClass("kiemel")
+  let tartalom = $(".tartalom").text();
+  console.log(tartalom);
 });
